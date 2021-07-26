89799105c43b7b0778e53551383f3d86cb7ce9a1
shun-inoue138 <ht8sk13@icloud.com>
2021-07-26 23:45:47 +0900

    remove-hard-corded-env-vars

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDNohTvDtLIiOzz5te35M6JHaEzo9K1khQ',
-  authDomain: 'realtime-chat-cb19e.firebaseapp.com',
-  projectId: 'realtime-chat-cb19e',
-  storageBucket: 'realtime-chat-cb19e.appspot.com',
-  messagingSenderId: '582115161175',
-  appId: '1:582115161175:web:e9c6a6c5d698755a045f12',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
